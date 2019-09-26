c9dc618b9d4bf9ef0a1d46920081807c65ce55ea
jantapabh <jantapa2407@gmail.com>
2019-09-26 21:18:44 +0700

    commit  7

--- BookContent.js
+++ BookContent.js
@@ -9,43 +9,15 @@ export default class BookContent extends React.Component {
         title: 'BookContent',
     };
 
-
-    constructor(props) {
-        super(props);
-        this.state = { text: '' };
-
-    }
-    onPressButton() {
-
-        Alert.alert('Thanks You Comments and Read Review .')
-
-
-    }
-
-
-
-
     render() {
         const { navigate } = this.props.navigation;
 
         const data = this.props.navigation.getParam('data')
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>{data.content} </Text>
-
-                <Text style={{ fontSize: 30, color: 'white', backgroundColor: '#669933', width: 400, justifyContent: 'center' }}> Comment This App </Text>
-
-                <TextInput
-                    style={{ height: 200, width: 400, backgroundColor: 'azure', fontSize: 20 }}
-                    placeholder="Comment about book this ! "
-                    onChangeText={(text) => this.setState({ text })}
-
-             />
-
+                <Text style={styles.container}>{data.content} </Text>
             </View> 
         );
-
-        <>
     }
 
     handlePress = () => {
