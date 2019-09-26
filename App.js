9656dcb92df8f33657542ad5b7a5173ae2e7199f
jantapabh <jantapa2407@gmail.com>
2019-09-26 21:02:16 +0700

    commit 4

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ import { createAppContainer } from "react-navigation";
 import HomeScreen from './HomeScreen';
 import Book from './Book';
 import BookContent from './BookContent';
-
+import ScoreBook from './ScoreBook';
 
 const Book1 = createStackNavigator({
 
   HomeScreen: { screen: HomeScreen },
   Book: { screen: Book },
   BookContent: { screen: BookContent },
+  ScoreBook: { screen: ScoreBook },
 
-  
 
 });
 
